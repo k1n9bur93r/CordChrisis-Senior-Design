8432392c9075a34d33e95b6bf8b0221139adc7e4
Nick Miller <diymovies@gmail.com>
2020-02-02 21:56:52 -0800

    Git assignment HelloWorld

---
+++ Miller.cs
@@ -0,0 +1,14 @@
+//Nicholas Miller 2001016608
+using System;
+using System.Collections.Generic;
+
+namespace HelloSpace
+{
+    public class HelloWorld
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
