ee2debfdcf3cbd65bcc230d55a1ce054d2659d9e
arjobosh <jarber22@gmail.com>
2020-02-03 14:03:19 -0800

    added hello world

---
+++ Assignment1/Arber.cs
@@ -0,0 +1,18 @@
+// Joshua Arber
+// CS 472
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloworld
+{
+    class Arber
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
