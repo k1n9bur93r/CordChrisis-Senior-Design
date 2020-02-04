3fffb5028af6f07552fc88d9023af0c4e9685a4f
Justin-Chadd S. Alasco <>
2020-02-03 17:06:13 -0800

    Create Alasco.cpp

---
+++ Assignment1/Alasco.cpp
@@ -0,0 +1,15 @@
+/*
+	Justin-Chadd S. Alasco
+	CS 472-1001
+	Assignment 1
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	cout << "Hello, world!" << endl;
+
+	return 0;
+}
