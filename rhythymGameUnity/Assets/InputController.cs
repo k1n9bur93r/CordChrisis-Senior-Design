996ec45f7ac16c39356653dee3ecc218353c185b
arjobosh <arber@unlv.nevada.edu>
2020-02-17 10:24:51 -0800

    input button testing

---
+++ rhythymGameUnity/Assets/InputController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    private Renderer r;
+    public KeyCode key;
+    private Color pressedColor;
+    private Color btnColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        r = GetComponent<Renderer>();
+        btnColor = r.material.color;
+        pressedColor = new Color(btnColor.r, btnColor.g + 0.4f, btnColor.b + 0.4f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(key))
+        {
+            r.material.color = pressedColor;
+        }
+        else if (Input.GetKeyUp(key))
+        {
+            r.material.color = btnColor;
+        }
+    }
+}
---
+++ rhythymGameUnity/Assets/NoteController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteController : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(0, -1.0f * Time.deltaTime, 0);
+        if (transform.position.y <= 0.375f && transform.position.y >= -0.375f && Input.GetKeyDown(KeyCode.Q))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
