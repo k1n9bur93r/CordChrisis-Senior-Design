61e3aa994044682a193385779a62ee291aebef10
Bryan Takemoto <takemb1@unlv.nevada.edu>
2020-02-18 13:52:37 -0800

    Initial 80s Map

---
+++ CC_Background/Assets/RowSet/Movement/Destroy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    public float lifeTime = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
