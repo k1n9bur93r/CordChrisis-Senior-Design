989e73d7a8ff420f88dbfddfe1c9abd667f64eae
Justin-Chadd S. Alasco <alasco@unlv.nevada.edu>
2020-02-17 20:11:59 -0800

    Asset folder reorganization
    Files that weren't already in a subfolder in the asset folder were moved to one. The Buttons folder was untouched.

---
+++ rhythymGameUnity/Assets/Scripts/NoteMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        rb.velocity = new Vector3 (0f,0f,-5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
