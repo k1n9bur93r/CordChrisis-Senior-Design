e8c724f346a18aa76c505fa41e22e3ba2b627d60
arjobosh <arber@unlv.nevada.edu>
2020-02-24 22:05:46 -0800

    added multiple colliders to input buttons, mouse functionality

---
+++ rhythymGameUnity/Assets/Scripts/Score.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public Text scoreText;
+    private GameObject input;
+    private int score;
+    NoteController s;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText = GetComponent<Text>();
+        input = GameObject.FindGameObjectWithTag("Note");
+        s = input.GetComponent<NoteController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = "Score: " + s.score.ToString();
+    }
+}
