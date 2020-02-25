d13cca76c4376fdc3fa06135b86b17001fa9d7bc
Aeren Dempsey <dempseyaeren@gmail.com>
2020-02-24 19:17:58 -0800

    refactored and cleaned up code

--- rhythymGameUnity/Assets/Scripts/Track.cs
+++ rhythymGameUnity/Assets/Scripts/Track.cs
@@ -16,14 +16,9 @@ class JsonTrack
 
 public class Track : MonoBehaviour
 {
-    public string track_file = "Text/basic";
+    public string track_file = "Text/more";
     public float note_spacing = 5;
     public float note_width = 5;
-
-    // public JsonTrack track
-
-    // private double[] note_locations;
-    // private GameObject[] notes;
 
     GameObject createNote(Vector3 position) {
         GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
@@ -31,38 +26,15 @@ public class Track : MonoBehaviour
         return cube;
     }
 
-    // double[] convertToPrimitive(List<double> list) {
-    //     note_locations = new double[list.Count];
-    //     int i = 0;
-    //     foreach (int entry in list)
-    //     {
-    //         note_locations[i] = entry;
-    //         i++;
-    //     }
-    //     return note_locations;
-    // }
-
-    JsonTrack readJsonFile() {
-        string json_string = Resources.Load<TextAsset>("Text/basic").ToString();
-        // Debug.Log(json_string);
-        // Debug.Log(json_text_file);
-        // Debug.Log(ta.ToString());
+    JsonTrack readJsonFile(string filename) {
+        string json_string = Resources.Load<TextAsset>(filename).ToString();
         JsonTrack json = JsonUtility.FromJson<JsonTrack>(json_string);
         return json;
     }
 
-    // Start is called before the first frame update
     void Start()
     {
-        // JsonTrack json = JsonUtility.FromJson<JsonTrack>("{\"notes\": [1, 1, 4, 1, 4, 3, 2, 1]}");
-        // note_locations = json.notes;
-        // Debug.Log("test");
-        // foreach (int entry in json) {
-        //     Debug.Log(entry);
-        // }
-        // note_locations = convertToPrimitive(json);
-
-        JsonTrack json = readJsonFile();
+        JsonTrack json = readJsonFile(track_file);
 
         int c = 0;
         foreach (double entry in json.notes) {
@@ -70,17 +42,5 @@ public class Track : MonoBehaviour
             createNote(position);
             c++;
         }
-
-        // notes = new GameObject[note_locations.Length];
-        // for (int i = 0; i < note_locations.Length; i++) {
-        //     Vector3 position = new Vector3((note_locations[i] - 1) * note_width, 0, i * note_spacing);
-        //     notes[i] = createNote(position);
-        // }
     }
-
-    // // Update is called once per frame
-    // void Update()
-    // {
-        
-    // }
 }
