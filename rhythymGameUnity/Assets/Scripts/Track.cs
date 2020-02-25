a291ffab4852c023b35cf6aea39b0d1f0f280686
Aeren Dempsey <dempseyaeren@gmail.com>
2020-02-24 19:11:43 -0800

    modified Track.cs class to use doubles for note locations and reading from and external file

--- rhythymGameUnity/Assets/Scripts/Track.cs
+++ rhythymGameUnity/Assets/Scripts/Track.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 class JsonTrack
 {
-    public int[] notes;
+    // Disable warnings of the form:
+    // 'JsonTrack.notes' is never assigned to, and will always have its default value null
+    // It gets assigned to in the json serializer
+    #pragma warning disable 0649
+    public double[] notes;
+    public double tempo;
+    public double offset;
+    #pragma warning restore 0649
 }
 
 public class Track : MonoBehaviour
 {
-    public int note_spacing = 5;
-    public int note_width = 5;
+    public string track_file = "Text/basic";
+    public float note_spacing = 5;
+    public float note_width = 5;
 
-    private int[] int_notes;
-    private GameObject[] notes;
+    // public JsonTrack track
 
+    // private double[] note_locations;
+    // private GameObject[] notes;
 
     GameObject createNote(Vector3 position) {
         GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
@@ -22,40 +31,56 @@ public class Track : MonoBehaviour
         return cube;
     }
 
-    int[] convertToPrimitive(List<int> list) {
-        int_notes = new int[list.Count];
-        int i = 0;
-        foreach (int entry in list)
-        {
-            int_notes[i] = entry;
-            i++;
-        }
-        return int_notes;
+    // double[] convertToPrimitive(List<double> list) {
+    //     note_locations = new double[list.Count];
+    //     int i = 0;
+    //     foreach (int entry in list)
+    //     {
+    //         note_locations[i] = entry;
+    //         i++;
+    //     }
+    //     return note_locations;
+    // }
+
+    JsonTrack readJsonFile() {
+        string json_string = Resources.Load<TextAsset>("Text/basic").ToString();
+        // Debug.Log(json_string);
+        // Debug.Log(json_text_file);
+        // Debug.Log(ta.ToString());
+        JsonTrack json = JsonUtility.FromJson<JsonTrack>(json_string);
+        return json;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        // int_notes = new JavaScriptSerializer().Deserialize<List<int>>("[1, 1, 4, 1, 4, 3, 2, 1]");
-        Debug.Log("test");
-        JsonTrack json = JsonUtility.FromJson<JsonTrack>("{\"notes\": [1, 1, 4, 1, 4, 3, 2, 1]}");
-        int_notes = json.notes;
+        // JsonTrack json = JsonUtility.FromJson<JsonTrack>("{\"notes\": [1, 1, 4, 1, 4, 3, 2, 1]}");
+        // note_locations = json.notes;
         // Debug.Log("test");
         // foreach (int entry in json) {
         //     Debug.Log(entry);
         // }
-        // int_notes = convertToPrimitive(json);
+        // note_locations = convertToPrimitive(json);
 
-        notes = new GameObject[int_notes.Length];
-        for (int i = 0; i < int_notes.Length; i++) {
-            Vector3 position = new Vector3((int_notes[i] - 1) * note_width, 0, i * note_spacing);
-            notes[i] = createNote(position);
+        JsonTrack json = readJsonFile();
+
+        int c = 0;
+        foreach (double entry in json.notes) {
+            Vector3 position = new Vector3((((float) entry) - 1) * note_width, 0, c * note_spacing);
+            createNote(position);
+            c++;
         }
+
+        // notes = new GameObject[note_locations.Length];
+        // for (int i = 0; i < note_locations.Length; i++) {
+        //     Vector3 position = new Vector3((note_locations[i] - 1) * note_width, 0, i * note_spacing);
+        //     notes[i] = createNote(position);
+        // }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    // // Update is called once per frame
+    // void Update()
+    // {
         
-    }
+    // }
 }
