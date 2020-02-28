0aea189b399d65c13ef1e01ed96692246ab9c195
Aaron Winkler <superhomeworkhelper@gmail.com>
2020-02-27 19:24:07 -0800

    test server: add login page

--- Website/testServer/testServer/ClientApp/src/App.js
+++ Website/testServer/testServer/ClientApp/src/App.js
@@ -5,6 +5,7 @@ import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
 import { LogIn } from './components/LogIn';
+import { SignUp } from './components/SignUp';
 
 import './custom.css'
 export default class App extends Component {
@@ -17,6 +18,7 @@ export default class App extends Component {
         <Route path='/counter' component={Counter} />
             <Route path='/fetch-data' component={FetchData} />
             <Route path='/login' component={LogIn} />
+            <Route path='/signup' component={SignUp} />
         </Layout>
     );
   }
