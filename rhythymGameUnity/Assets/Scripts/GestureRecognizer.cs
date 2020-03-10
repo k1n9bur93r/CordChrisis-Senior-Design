4a556da4a85deaf380202c90540fb648cd0a8bb2
Aeren Dempsey <dempseyaeren@gmail.com>
2020-03-10 11:58:14 -0700

    added gesture recognizer

---
+++ rhythymGameUnity/Assets/Scripts/GestureRecognizer.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GestureRecognizer : MonoBehaviour
+{
+    private Vector3 start_position = Vector3.zero;
+    private bool has_swiped = false;
+
+    private float abs(float x) {return (x < 0) ? -x : x;}
+
+    // Update is called once per frame
+    // void Update()
+    // {
+    //     // must store response of IsSwipe() because it will only return once with the swipe direction
+    //     string swipe = IsSwipe();
+    //     if (swipe != "") {
+    //         Debug.Log(swipe);
+    //     }
+    // }
+
+    void OnMouseDown()
+    {
+        start_position = Input.mousePosition;
+        // Debug.Log(Input.mousePosition);
+    }
+
+    void OnMouseUp()
+    {
+        has_swiped = true;
+    }
+
+    public string IsSwipe()
+    {
+        // only send each swipe once
+        // swipe is ended on mouse up
+        if (!has_swiped) {
+            return "";
+        }
+        has_swiped = false;
+
+        Vector3 delta_position = Input.mousePosition - start_position;
+        if (abs(delta_position.x) > abs(delta_position.y))
+        {
+            if (delta_position.x > 0)
+            {
+                return "right";
+            }
+            else
+            {
+                return "left";
+            }
+        }
+        else
+        {
+            if (delta_position.y > 0)
+            {
+                return "up";
+            }
+            else
+            {
+                return "down";
+            }
+        }
+    }
+}
