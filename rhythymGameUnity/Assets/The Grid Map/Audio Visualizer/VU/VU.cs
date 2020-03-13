b92d14213cf7bae74c010bdc0ab4aadae0653df3
Bryan Takemoto <35548639+AkagiCoder@users.noreply.github.com>
2020-03-13 03:26:44 -0700

    Properly set up the VU meters.

--- rhythymGameUnity/Assets/The Grid Map/Audio Visualizer/VU/VU.cs
+++ rhythymGameUnity/Assets/The Grid Map/Audio Visualizer/VU/VU.cs
@@ -4,21 +4,29 @@ using UnityEngine;
 
 public class VU : MonoBehaviour
 {
+    public bool invertDirection;
     public float maxScale;
     public GameObject sampleCubePrefab;
     GameObject[] sampleCube = new GameObject[AudioSpectrum.sampleRange];
     // Start is called before the first frame update
     void Start()
     {
-        for(int i = 0; i < AudioSpectrum.sampleRange; i++)
+        int inv = 1;
+        if(invertDirection)
+        {
+            inv = -1;
+        }
+        Vector3 currPos = this.transform.position;
+        for (int i = 0; i < AudioSpectrum.sampleRange; i++)
         {
             GameObject instanceSampleCube = (GameObject)Instantiate(sampleCubePrefab);
-            instanceSampleCube.transform.position = this.transform.position;
+            instanceSampleCube.transform.position = currPos;
             instanceSampleCube.transform.parent = this.transform;
             instanceSampleCube.name = "SampleCube" + i;
-            this.transform.eulerAngles = new Vector3(0, -0.703125f * i, 0);
-            instanceSampleCube.transform.position = Vector3.forward * 100;
+            //this.transform.eulerAngles = new Vector3(0, -0.703125f * i, 0);
+            //instanceSampleCube.transform.position = Vector3.forward * 100;
             sampleCube[i] = instanceSampleCube;
+            currPos += new Vector3(1 * inv, 0, 0);
         }
     }
 
---
+++ rhythymGameUnity/Assets/The Grid Map/Audio Visualizer/VU/VU_Behavior.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VU_Behavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = new Vector3(transform.localPosition.x, (transform.localScale.y - 1) / 2, transform.localPosition.z);
+    }
+}
