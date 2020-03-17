f2e7e737273ac339c1b9a860251fded9510003d3
Bryant Pochop <pochop@unlv.nevada.edu>
2020-03-16 18:17:35 -0700

    added userlog.razor, userlogin.controller. login..cs

---
+++ Website/CordChrisis/CordChrisis/Server/Controllers/UserloginController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CordChrisis.Shared.Models;
+
+namespace CordChrisis.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserLoginController : Controller
+    {
+        [HttpPost]
+        [Route("post")]
+        public Boolean GetLogin([FromBody] Login postData)
+        {
+
+
+            Console.WriteLine("are we gettting here?");
+            Console.WriteLine("\n\n\n\n\n\n\n");
+
+
+            return true;
+        }
+        //public IActionResult GetDefaultSearchList()
+        //{
+        //    return View();
+        //}
+    }
+}
+
