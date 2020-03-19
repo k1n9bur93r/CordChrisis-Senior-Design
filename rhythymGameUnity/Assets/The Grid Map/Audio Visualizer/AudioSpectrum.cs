2450a4d51d7111e2ede18829e0fee19572016c02
Bryan Takemoto <35548639+AkagiCoder@users.noreply.github.com>
2020-03-18 20:59:45 -0700

    Recommiting "The Grid Map".

---
+++ rhythymGameUnity/Assets/The Grid Map/Audio Visualizer/AudioSpectrum.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (AudioSource))]
+
+public class AudioSpectrum : MonoBehaviour
+{
+    AudioSource audioSource;
+    public float meterDropSpeed = 0.01f;
+    public static int sampleRange = 512;
+    public static float[] samples = new float[sampleRange];    // Stores the frequency sample
+    public static float[] freqBand = new float[8];
+    public static float[] bandBuffer = new float[8];
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetSpectrumAudioSource();
+        MakeFrequencyBands();
+        BandBuffer();
+    }
+
+    void GetSpectrumAudioSource()
+    {
+        audioSource.GetSpectrumData(samples, 0, FFTWindow.Blackman);
+    }
+
+    void MakeFrequencyBands()
+    {
+        int count = 0;
+        float average;
+
+        for(int i = 0; i < 8; i++)
+        {
+            average = 0;
+            int sampleCount = (int)Mathf.Pow(2, i) * 2;
+            // 7 Frequency Bands
+            if(i == 7)
+            {
+                sampleCount += 2;
+            }
+
+            for(int j = 0; j < sampleCount; j++)
+            {
+                average += samples[count] * (count + 1);
+                count++;
+            }
+            average /= count;
+            freqBand[i] = average * 10;
+        }
+    }
+
+    void BandBuffer()
+    {
+        for(int i = 0; i < 8; i++)
+        {
+            // Assign the bandBuffer to the value of freqBand if it's lower than it
+            if(bandBuffer[i] < freqBand[i])
+            {
+                bandBuffer[i] = freqBand[i];
+            }
+            else if(bandBuffer[i] >= freqBand[i] && bandBuffer[i] > 0)
+            {
+                bandBuffer[i] -= meterDropSpeed;
+            }
+        }
+    }
+}
---
+++ rhythymGameUnity/Assets/The Grid Map/Audio Visualizer/VU/VU.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VU : MonoBehaviour
+{
+    public bool invertDirection;
+    public float maxScale;
+    public GameObject sampleCubePrefab;
+    GameObject[] sampleCube = new GameObject[AudioSpectrum.sampleRange];
+    // Start is called before the first frame update
+    void Start()
+    {
+        int inv = 1;
+        if(invertDirection)
+        {
+            inv = -1;
+        }
+        Vector3 currPos = this.transform.position;
+        for (int i = 0; i < AudioSpectrum.sampleRange; i++)
+        {
+            GameObject instanceSampleCube = (GameObject)Instantiate(sampleCubePrefab);
+            instanceSampleCube.transform.position = currPos;
+            instanceSampleCube.transform.parent = this.transform;
+            instanceSampleCube.name = "SampleCube" + i;
+            //this.transform.eulerAngles = new Vector3(0, -0.703125f * i, 0);
+            //instanceSampleCube.transform.position = Vector3.forward * 100;
+            sampleCube[i] = instanceSampleCube;
+            currPos += new Vector3(1 * inv, 0, 0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = 0; i < AudioSpectrum.sampleRange; i++)
+        {
+            if(sampleCube != null)
+            {
+                sampleCube[i].transform.localScale = new Vector3(1, (AudioSpectrum.samples[i] * maxScale) + 2, 1);
+            }
+        }
+    }
+}
