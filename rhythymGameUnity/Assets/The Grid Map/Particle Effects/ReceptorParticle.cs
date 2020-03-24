5a3f53878fa4dd33710dd5fca88721914e4042c1
Bryan Takemoto <35548639+AkagiCoder@users.noreply.github.com>
2020-03-23 20:16:25 -0700

    Receptor particle effect.

---
+++ rhythymGameUnity/Assets/The Grid Map/Particle Effects/ReceptorParticle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReceptorParticle : MonoBehaviour
+{
+    private static bool splash = false;
+    public ParticleSystem particleLauncher;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButton("Fire1"))
+        {
+            splash = true;
+        }
+        if (splash)
+        {
+            particleLauncher.Emit(1);
+            splash = false;
+        }
+    }
+}
