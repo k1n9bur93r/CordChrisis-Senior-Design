94a6420b549ae8dc5ef346fe98c2db89182c5c64
Bryan Takemoto <35548639+AkagiCoder@users.noreply.github.com>
2020-03-24 22:29:03 -0700

    Added a prewarm grid at the start of the game.

--- rhythymGameUnity/Assets/The Grid Map/Grid/GridSpawner.cs
+++ rhythymGameUnity/Assets/The Grid Map/Grid/GridSpawner.cs
@@ -14,6 +14,12 @@ public class GridSpawner : MonoBehaviour
         go.AddComponent<GridWave>();
         go.transform.localScale = new Vector3(GScale, GScale, 100);
         Destroy(go, 100f);
+
+        Vector3 initOff = new Vector3(0, 0, -600);
+        GameObject go2 = Instantiate(Grid, StartPos.position + initOff, StartPos.rotation);
+        go2.AddComponent<GridWave>();
+        go2.transform.localScale = new Vector3(GScale, GScale, 100);
+        Destroy(go, 100f);
     }
     // Update is called once per frame
     void Update()
